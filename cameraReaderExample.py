5fd0aa2e4088212ff18e60804d811349b5543732
Edgars Mjasnikovs <EMJA@JWinc.jeld-wen.com>
2018-02-16 14:16:14 +0200

    Update

--- cameraReaderExample.py
+++ cameraReaderExample.py
@@ -12,6 +12,7 @@ parser.add_argument('--width', type=int, help='Camera resolution - width')
 parser.add_argument('--height', type=int, help='Camera resolution - height')
 parser.add_argument('--fps', type=int, help='Target camera FPS')
 parser.add_argument('--opencl', type=int, help='Use opencl')
+parser.add_argument('--name', type=str, help='Camera name')
 
 args = parser.parse_args()
 
@@ -19,10 +20,12 @@ WINDOWS_NAME = 'camera reader example'
 SCALE_ARGS = 1 if (args.downscale is None) else args.downscale
 WIDTH_ARGS = 4096 if (args.width is None) else args.width
 HEIGHT_ARGS = 2160 if (args.height is None) else args.height
-FPS_ARGS = 120 if (args.fps is None) else args.fps
+FPS_ARGS = 30 if (args.fps is None) else args.fps
+NAME_ARGS = 'camera' if (args.name is None) else args.name
 
-MATRIX = np.loadtxt('calibrations/brio1_matrix.txt', delimiter=',')
-DISTORTION = np.loadtxt('calibrations/brio1_distortion.txt', delimiter=',')
+MATRIX = np.loadtxt('calibrations/' + NAME_ARGS + '_matrix.txt', delimiter=',')
+DISTORTION = np.loadtxt('calibrations/' + NAME_ARGS + '_distortion.txt',
+                        delimiter=',')
 
 stream = webcamStream(1, WIDTH_ARGS, HEIGHT_ARGS, FPS_ARGS).start()
 stream.calibrateCamera(MATRIX, DISTORTION)
@@ -34,6 +37,15 @@ while True:
     frame = stream.readClean()
 
     if frame is not None:
+        frame = cv.UMat(frame)
+
+        frame = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+        frame = cv.GaussianBlur(frame, (1, 1), 0)
+        frame = cv.bilateralFilter(frame, 9, 75, 75)
+        frame = cv.Canny(frame, 0, 255)
+
+        frame = cv.cvtColor(cv.UMat(frame).get(), cv.COLOR_GRAY2BGR)
+
         if (SCALE_ARGS > 1):
             frame = downScaleImage(frame, SCALE_ARGS)
 
@@ -42,7 +54,7 @@ while True:
                            cv.FONT_HERSHEY_SIMPLEX, 0.50, (0, 255, 0), 1)
 
         cv.imshow(WINDOWS_NAME, frame)
-        k = cv.waitKey(100)
+        k = cv.waitKey(int(1000 / FPS_ARGS))
 
         if k == 27:
             break
